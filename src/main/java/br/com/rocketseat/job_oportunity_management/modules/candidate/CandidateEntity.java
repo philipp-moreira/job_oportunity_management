090318acce44021748a40f0b68e5fa5190fbf8fb
Philipp Moreira <psmdeveloper@gmail.com>
2024-02-28 14:48:09 -0300

    feat: insert validation to candidate
    #class_02

--- src/main/java/br/com/rocketseat/job_oportunity_management/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/rocketseat/job_oportunity_management/modules/candidate/CandidateEntity.java
@@ -2,14 +2,26 @@ package br.com.rocketseat.job_oportunity_management.modules.candidate;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class CandidateEntity {
     UUID id;
     String name;
+
+    @Pattern(regexp = "^\\S*$", message = "must don't have blank spaces.")
+    @NotBlank
     String username;
+
+    @Email(message = "The attribute must be have a valid e-mail.")
     String email;
+
+    @Length(min = 10, max = 100)
     String password;
     String description;
     String curriculum;
--- src/main/java/br/com/rocketseat/job_oportunity_management/modules/candidate/controller/CandidateController.java
+++ src/main/java/br/com/rocketseat/job_oportunity_management/modules/candidate/controller/CandidateController.java
@@ -6,13 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.rocketseat.job_oportunity_management.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
     @PostMapping
-    public void create(@RequestBody CandidateEntity candidate) {
+    public void create(@Valid @RequestBody CandidateEntity candidate) {
         System.out.println("Candidate");
         System.out.println(candidate.getEmail());
     }
