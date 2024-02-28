ddb32a3be606de9d4209bb94f691edfe635baa8e
Philipp Moreira <psmdeveloper@gmail.com>
2024-02-28 18:14:28 -0300

    feat: create candidate's repository
    - create candidate's repository, using JPA mechanism, just extending interface JpaRepository<T, TId>
    - using the new repository in the respective controller
    
    #class_06

---
+++ src/main/java/br/com/rocketseat/job_oportunity_management/modules/candidate/CandidateRepository.java
@@ -0,0 +1,8 @@
+package br.com.rocketseat.job_oportunity_management.modules.candidate;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
+}
--- src/main/java/br/com/rocketseat/job_oportunity_management/modules/candidate/controller/CandidateController.java
+++ src/main/java/br/com/rocketseat/job_oportunity_management/modules/candidate/controller/CandidateController.java
@@ -1,20 +1,25 @@
 package br.com.rocketseat.job_oportunity_management.modules.candidate.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.rocketseat.job_oportunity_management.modules.candidate.CandidateEntity;
+import br.com.rocketseat.job_oportunity_management.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
+    
+    @Autowired
+    CandidateRepository candidateRepository;
+
 
     @PostMapping
-    public void create(@Valid @RequestBody CandidateEntity candidate) {
-        System.out.println("Candidate");
-        System.out.println(candidate.getEmail());
+    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidate) {
+        return candidateRepository.save(candidate);
     }
 }
