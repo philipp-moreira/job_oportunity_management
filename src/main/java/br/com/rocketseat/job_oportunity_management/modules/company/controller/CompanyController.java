2e6c07caced174f464552bfdbf9a9f6ebe345fc1
Philipp Moreira <psmdeveloper@gmail.com>
2024-02-29 16:09:23 -0300

    feat: create company contrroller
    #class_12

---
+++ src/main/java/br/com/rocketseat/job_oportunity_management/modules/company/controller/CompanyController.java
@@ -0,0 +1,30 @@
+package br.com.rocketseat.job_oportunity_management.modules.company.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.rocketseat.job_oportunity_management.modules.company.entity.CompanyEntity;
+import br.com.rocketseat.job_oportunity_management.modules.company.usecase.CreateCompanyUseCase;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/company")
+public class CompanyController {
+
+    @Autowired
+    CreateCompanyUseCase createCompanyUseCase;
+
+    @PostMapping
+    public ResponseEntity<Object> create(@Valid @RequestBody CompanyEntity company) {
+        try {
+            var result = createCompanyUseCase.execute(company);
+            return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
