ef6346f88ae1d8f714897fc3ac68c6328f2ffd91
Philipp Moreira <psmdeveloper@gmail.com>
2024-03-01 09:18:12 -0300

    feat: add hash to candidate's password
    #class_21

--- src/main/java/br/com/rocketseat/job_oportunity_management/modules/candidate/usecase/CreateCandidateUseCase.java
+++ src/main/java/br/com/rocketseat/job_oportunity_management/modules/candidate/usecase/CreateCandidateUseCase.java
@@ -1,6 +1,7 @@
 package br.com.rocketseat.job_oportunity_management.modules.candidate.usecase;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.rocketseat.job_oportunity_management.exceptions.UserFoundException;
@@ -13,12 +14,18 @@ public class CreateCandidateUseCase {
     @Autowired
     CandidateRepository candidateRepository;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidate) {
         candidateRepository
                 .findByUsernameOrEmail(candidate.getUsername(), candidate.getEmail())
                 .ifPresent((user) -> {
                     throw new UserFoundException();
                 });
+
+        var passwordEncoded = passwordEncoder.encode(candidate.getPassword());
+        candidate.setPassword(passwordEncoded);
 
         return candidateRepository.save(candidate);
     }
