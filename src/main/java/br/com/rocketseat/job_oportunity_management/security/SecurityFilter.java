62c94cb39c028c5899d836868e27775c65c526da
Philipp Moreira <psmdeveloper@gmail.com>
2024-03-01 08:55:03 -0300

    feat: validate token
    - completed implementation to filter class
    - created class to validate JWT
    
    #class_19

--- src/main/java/br/com/rocketseat/job_oportunity_management/security/SecurityFilter.java
+++ src/main/java/br/com/rocketseat/job_oportunity_management/security/SecurityFilter.java
@@ -1,23 +1,47 @@
 package br.com.rocketseat.job_oportunity_management.security;
 
 import java.io.IOException;
+import java.util.Collections;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import br.com.rocketseat.job_oportunity_management.provider.JWTProvider;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class SecurityFilter  extends OncePerRequestFilter {
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    JWTProvider jwtProvider;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+
         var authorizationFromHeaderRequest = request.getHeader("Authorization");
-        System.out.println(authorizationFromHeaderRequest);
+        if (authorizationFromHeaderRequest != null) {
+
+            var subjectJWT = jwtProvider.validateToken(authorizationFromHeaderRequest);
+            if (subjectJWT.isEmpty()) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+
+            request.setAttribute("company_id", subjectJWT);
+
+            var contextAuth = new UsernamePasswordAuthenticationToken(subjectJWT, null, Collections.emptyList());
+
+            SecurityContextHolder
+                    .getContext()
+                    .setAuthentication(contextAuth);
+        }
 
         filterChain.doFilter(request, response);
     }
