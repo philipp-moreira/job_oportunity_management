86d648eff16fcb2dfc392b573a846f7cd5575c3d
Philipp Moreira <psmdeveloper@gmail.com>
2024-03-01 08:23:27 -0300

    feat: recover JWT
    - add specializing filter to spring boot
    - started the implementation of filter
    
    #class_18

--- src/main/java/br/com/rocketseat/job_oportunity_management/security/SecurityConfig.java
+++ src/main/java/br/com/rocketseat/job_oportunity_management/security/SecurityConfig.java
@@ -1,14 +1,19 @@
 package br.com.rocketseat.job_oportunity_management.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    SecurityFilter securityFilter;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -21,7 +26,9 @@ public class SecurityConfig {
                             .requestMatchers("/auth/company").permitAll()
                             .anyRequest().authenticated();
                 })
+                .addFilterBefore(securityFilter, BasicAuthenticationFilter.class)
                 .build();
+
     }
 
     @Bean
