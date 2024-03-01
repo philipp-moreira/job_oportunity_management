3f90755bda8f04f4c46bbe591ff6bacf572de9fa
Philipp Moreira <psmdeveloper@gmail.com>
2024-03-01 09:11:53 -0300

    feat: refactor job controller
    - refactored job entity to use facilities from lombok package to build objects from class
    - created data transfer object (DTO) to job class, to minimize data exposition
    - added mecanhism to job controller to recover company Id do JWT (security layer) to complement the object to persist in database
    
    #class_20

--- src/main/java/br/com/rocketseat/job_oportunity_management/modules/company/controller/JobController.java
+++ src/main/java/br/com/rocketseat/job_oportunity_management/modules/company/controller/JobController.java
@@ -1,5 +1,7 @@
 package br.com.rocketseat.job_oportunity_management.modules.company.controller;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,8 +9,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.rocketseat.job_oportunity_management.modules.company.dto.CreateJobDTO;
 import br.com.rocketseat.job_oportunity_management.modules.company.entity.JobEntity;
 import br.com.rocketseat.job_oportunity_management.modules.company.usecase.CreateJobUseCase;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 @RestController
@@ -19,10 +23,23 @@ public class JobController {
     CreateJobUseCase createJobUseCase;
 
     @PostMapping
-    public ResponseEntity<Object> create(@Valid @RequestBody JobEntity job) {
+    public ResponseEntity<Object> create(@Valid @RequestBody CreateJobDTO job, HttpServletRequest request) {
         try {
-            var result = createJobUseCase.execute(job);
+            var companyId = request.getAttribute("company_id");
+            var companyIdParsed = UUID.fromString(companyId.toString());
+
+            var jobEntity = JobEntity
+                    .builder()
+                    .companyID(companyIdParsed)
+                    .level(job.getLevel())
+                    .benefits(job.getBenefits())
+                    .description(job.getDescription())
+                    .build();
+
+            var result = createJobUseCase.execute(jobEntity);
+
             return ResponseEntity.ok().body(result);
+
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
---
+++ src/main/java/br/com/rocketseat/job_oportunity_management/modules/company/dto/CreateJobDTO.java
@@ -0,0 +1,10 @@
+package br.com.rocketseat.job_oportunity_management.modules.company.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateJobDTO {
+    String level;
+    String benefits;
+    String description;
+}
